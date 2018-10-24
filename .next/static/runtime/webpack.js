4fb1f21b2c9a6ff92fda83082bd838424c0a9087
jacobsilver2 <jacobsilver2@mac.com>
2018-10-24 10:44:24 -0400

    start ch 17

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2308739fd6063ebe1a4d";
+/******/ 	var hotCurrentHash = "5d8d640e2228516d1f30";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
