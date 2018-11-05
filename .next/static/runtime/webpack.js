3e7288e28b116e453ed0194abaf1ae1fc238c12f
jacobsilver2 <jacobsilver2@mac.com>
2018-11-05 04:15:55 -0500

    fixed some styling in table

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bdaa1cac991105d2a9f5";
+/******/ 	var hotCurrentHash = "a5d7846fdb730a7cb382";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
